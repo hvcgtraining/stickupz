6dea57e5e1963dfbba4d1c2ada31bcd096cea9bd
dongnguyen <nguyentandong0511@gmai.com>
2019-06-26 14:47:43 +0700

    add

--- project/dist/js/animate.js
+++ project/dist/js/animate.js
@@ -2,13 +2,11 @@
 
 var modal = document.querySelectorAll('.service-modal');
 var btnList = document.querySelectorAll('.modal-content  button.btn-modal');
-var header = document.getElementById('header');
 document.addEventListener('DOMContentLoaded', function () {
   for (var i = 0; i < btnList.length; i++) {
     btnList[i].addEventListener('click', function () {
       var indexParent = this.parentElement.parentElement.nextSibling;
       indexParent.style.display = 'block';
-      header.classList.add('zIndex');
     });
   }
 
@@ -16,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
     for (var i = 0; i < modal.length; i++) {
       if (event.target === modal[i]) {
         modal[i].style.display = 'none';
-        header.classList.remove('zIndex');
       }
     }
   });
